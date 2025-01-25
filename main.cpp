45231f1833a854cd9f18ec1c3cca8297ffe578be
Christian McKenzie <Christian McKenzie>
2025-01-24 21:18:32 -0600

    added new features!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+int sum(int n){
+  int total = 0;
+  for(int i = 0; i<= n; i++){
+    total += i;
+  }
+  return total;
+}
+
 int main(){
-  std::cout << "Hello World!" << std::endl;
+  int n;
+  std::cout << "Enter a #: ";
+  std::cin >> n;
+  std::cout <<"The sum of #'s from 1 to " << n << " is " << sum(n) << std::endl;
   return 0;
 }
